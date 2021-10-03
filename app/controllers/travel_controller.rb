5ecb49255b93af15e0acdd37f001e473d5a963e4
Jenny Moon <jenny0moon@gmail.com>
2021-10-03 13:09:04 +0100

    Add request_api method

--- app/controllers/travel_controller.rb
+++ app/controllers/travel_controller.rb
@@ -9,4 +9,26 @@ class TravelController < ApplicationController
       flash[:alert] = 'country not found'
       return render action: :index
   end
+
+private
+
+  def request_api(url)
+    response = Excon.get(
+      url,
+      headers: {
+        'X-RapidAPI-Host' => URI.parse(url).host,
+        'X-RapidAPI-Key' => ENV.fetch('RAPIDAPI_API_KEY')
+      }
+    )
+
+    return nil if response.status != 200
+
+    JSON.parse(response.body)
+  end
+
+  def find_country(name)
+    request_api(
+      "https://restcountries-v1.p.rapidapi.com/name/#{URI.encode(name)}"
+    )
+  end
 end
