07d7d60362cb39cd7a9d12797868a68d4851d07f
Jenny Moon <jenny0moon@gmail.com>
2021-10-03 13:00:56 +0100

    Add search method

--- app/controllers/travel_controller.rb
+++ app/controllers/travel_controller.rb
@@ -1,4 +1,12 @@
 class TravelController < ApplicationController
   def index
   end
+
+  def search
+    countries = find_country(params[:country])
+
+    unless countries
+      flash[:alert] = 'country not found'
+      return render action: :index
+  end
 end
